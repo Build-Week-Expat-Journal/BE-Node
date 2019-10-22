f87ba4b8f0e496b02fbbd339e27fc21d97aaa3ee
Rockey Simmons <codenamerockey@gmail.com>
2019-10-21 18:02:59 -0700

    updated user-messages seed file

--- data/seeds/007-user-messages.js
+++ data/seeds/007-user-messages.js
@@ -1,8 +1,8 @@
 exports.seed = function(knex) {
-  // Inserts seed entries
-  return knex('user_message').insert([
-    { user_id: 1, message_id: 1 },
-    { user_id: 2, message_id: 1 },
-    { user_id: 3, message_id: 3 }
-  ]);
+  // // Inserts seed entries
+  // return knex('user_message').insert([
+  //   { user_id: 1, message_id: 1 },
+  //   { user_id: 2, message_id: 1 },
+  //   { user_id: 3, message_id: 3 }
+  // ]);
 };
