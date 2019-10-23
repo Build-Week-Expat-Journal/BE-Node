07c6df3ad6f858330910997249992f636060bc31
Rockey Simmons <codenamerockey@gmail.com>
2019-10-23 10:45:09 -0700

    added middleware for interests end point and messages endpoint

--- messages/messageRouter.js
+++ messages/messageRouter.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
-
+const authMiddleware = require('../auth-middleware/authMiddleWare.js');
 const getMessages = require('../messages/message_db_helpers.js');
 
-router.get('/', (req, res) => {
+router.get('/', authMiddleware, (req, res) => {
   getMessages.find().then(message => {
     res.status(200).json(message);
   });
